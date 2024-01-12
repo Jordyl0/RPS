6be1c04d75918b038522382b49476d10ca8c0648
Jordan <jordanleedham@gmail.com>
2024-01-11 21:43:50 -0800

    Creating a round system

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let playerScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice() {
@@ -7,5 +8,30 @@ function getComputerChoice() {
     return randomChoice;
     
 }
+playerSelection = window.prompt("What is your choice?", "");
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection)
+    {
+        return console.log("It's a tie");
+    
+    }
+    else if ((playerSelection.toUpperCase() === "rock" && computerSelection.toUpperCase() === "scissors") ||
+    (playerSelection.toUpperCase() === "scissors" && computerSelection.toUpperCase() === "paper") ||
+    (playerSelection.toUpperCase() === "paper" && computerSelection.toUpperCase() === "rock") 
+    ) {
+        playerScore++
+        return "Player Wins"
+    }
+   else if ( (computerSelection.toUpperCase() === "rock" && playerSelection.toUpperCase() === "scissors") ||
+    (computerSelection.toUpperCase() === "scissors" && playerSelection.toUpperCase() === "paper") || 
+    (computerSelection.toUpperCase() === "paper" && playerSelection.toUpperCase() === "rock") 
+    ) {
+        computerScore++
+        return console.log("Comp Wins")
+    }
+    
+     
+}
+console.log(playRound());
+
