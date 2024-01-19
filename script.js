7a201af6839396cfa0286f1457aa783ff51fd92b
Jordan <jordanleedham@gmail.com>
2024-01-18 22:13:49 -0800

    Added new checkWinner Func

--- script.js
+++ script.js
@@ -1,56 +1,96 @@
-let playerScore = 0;
-let computerScore = 0;
 
+const options = [ "rock", "paper", "scissors"];
 // Create function to make random selection in RPS
 
 function getComputerChoice() {
-    const options = [ "rock", "paper", "scissors"];
-    let randomChoice = options[(Math.floor(Math.random() *  options.length))]; 
-    return randomChoice;
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+
+
+function checkWinner(playerSelection, computerSelection){
+if(playerSelection == computerSelection){
+    return "Tie"
+}
+else if(
+     (playerSelection == "rock" && computerSelection  == "scissors") ||
+    (playerSelection  == "scissors" && computerSelection  == "paper") ||
+    (playerSelection  == "paper" && computerSelection  == "rock")) 
+    {
+     return "Player"
+     
+    }
+    else {
+        return "Computer";
+    }
+        
+    
     
 }
-let playerSelection = prompt("What is your choice?");
-let computerSelection = getComputerChoice();
 
+ 
 
 function playRound (playerSelection, computerSelection)
-
-{
-   if (playerSelection == computerSelection)
-   {
-       return ("its a tie");
-   }
-   else if (playerSelection.toString() === "rock" && computerSelection.toString()  === "scissors")  {
-       playerScore++
-       return("player win");
-   }
-else if
-   (playerSelection.toString()  === "scissors" && computerSelection.toString()  === "paper") {
-       playerScore++
-       return("player win");
-   }
-else if
-   (playerSelection.toString()  === "paper" && computerSelection.toString()  === "rock")
-    {
-       playerScore++
-       return("player win");
-   }
-   else if ((playerSelection.toString()  === "scissors" && computerSelection.toString()  === "rock") ){
-       computerScore++
-   return("comp wins");
-}    
- else if  (playerSelection.toString()  === "rock" && computerSelection.toString()  === "paper") {
-   computerScore++
-   return("comp wins");
+{ const result = checkWinner(playerSelection,computerSelection)
+ if(result == "Tie") {
+    return "It's a tie"
  }
- else if
-(playerSelection,toString()  === "paper" && computerSelection.toString()  === "scissors") {
-   computerScore++
-   return("comp wins");
-}
-}
+ else if (result == "Player"){
+    return `You Win ! ${playerSelection} beats ${computerSelection}`
+ }
+ else {
+    return `You Lose ! ${computerSelection} beats ${playerSelection}`
+ }
+   
+  }
+   
+  function getPlayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const playerChoice = window.prompt("Rock Paper Scissors");
+        if(playerChoice == null){
+           continue;
+        }
+        const choiceInLower = playerChoice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+
+    }
+  }
+
+  
 
 
-console.log(getComputerChoice());
-console.log(playRound(playerSelection, computerSelection));
+  function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection));
+        if (checkWinner(playerSelection,computerSelection) == "Player"){
+            playerScore++;
+        }
+        else if(checkWinner(playerSelection,computerSelection) == "Computer"){
+            computerScore++;
+        }
+    }
+    if(playerScore > computerScore) {
+        console.log("Player wins!");
+    }
+    else if(playerScore < computerScore){
+        console.log("Computer wins!");
+    }
+    else {
+        console.log("Run it back!");
+    }
+  }
+
+  
+
+
+game()
 
